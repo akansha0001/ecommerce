1cf3012411b3d9d16c9732c1d4e7fb94cdca5655
akansha0001 <b116006@iiit-bh.ac.in>
2020-05-14 19:25:03 +0530

    add orderss

--- src/orders/models.py
+++ src/orders/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from carts.models import Cart
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save,post_save
 from ecommerce.utils  import unique_order_id_generator
 
 # Create your models here.
@@ -32,10 +32,36 @@ class Order(models.Model):
     def __str__(self):
         return self.order_id
 
-
+    def update_total(self):
+        return
 
 def pre_save_create_order_id(sender, instance, *args, **kwargs):
     if not instance.order_id:
         instance.order_id = unique_order_id_generator(instance)
 
+
+def post_save_cart_total(sender, instance, *args, **kwargs):
+    if not Created:
+        cart_obj=instance
+        cart_total=cart_obj.total
+        cart_id=cart_obj.id
+        qs=Orders.objects.filter(cart__id=cart_id)
+        if qs.count()==1 :
+            order_obj=qs.first()
+            order_obj.update_total()
+
+
+    
 pre_save.connect(pre_save_create_order_id, sender=Order)
+post_save.connect(post_save_cart_total, sender=Cart)
+
+
+def post_save_order(sender, instance, created, *args, **kwargs):
+    print("running")
+    if created:
+        print("Updating... first")
+        instance.update_total()
+
+
+post_save.connect(post_save_order, sender=Order)
+
