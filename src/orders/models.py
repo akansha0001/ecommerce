65a3c15eace6a4f85e4d17495b5909c025bb0f7e
akansha0001 <b116006@iiit-bh.ac.in>
2020-05-15 18:13:43 +0530

    add guest user

--- src/orders/models.py
+++ src/orders/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from carts.models import Cart
 from django.db.models.signals import pre_save,post_save
 from ecommerce.utils  import unique_order_id_generator
-
+from billing.models import BillingProfile
 import math
 
 # Create your models here.
@@ -15,20 +15,38 @@ ORDER_STATUS_CHOICES=(
     ('refunded','Refunded')
 )
 #order_id 
+
+class OrderManager(models.Manager):
+    def new_or_get(self, billing_profile, cart_obj):
+        created = False
+        qs = self.get_queryset().filter(
+                billing_profile=billing_profile, 
+                cart=cart_obj, 
+                active=True)
+        if qs.count() == 1:
+            obj = qs.first()
+        else:
+            obj = self.model.objects.create(
+                    billing_profile=billing_profile, 
+                    cart=cart_obj)
+            created = True
+        return obj, created
+
+
+
 class Order(models.Model):
-    
+    billing_profile = models.ForeignKey(BillingProfile, null=True, blank=True,on_delete=models.CASCADE)
+    order_id        = models.CharField(max_length=120, blank=True) # AB31DE3
+    # billing_profile = ?
+    # shipping_address
+    # billing_address 
+    cart            = models.ForeignKey(Cart,on_delete=models.CASCADE)
+    status          = models.CharField(max_length=120, default='created', choices=ORDER_STATUS_CHOICES)
+    shipping_total  = models.DecimalField(default=5.99, max_digits=100, decimal_places=2)
+    total           = models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
+    active          = models.BooleanField(default=True)
 
-    ''' set of random string'''
-    order_id            =models.CharField(max_length=120,blank=True)
-    # billing_profile     =
-
-    # shipping_address    =
-    # billing_address     =
-    cart                =models.ForeignKey(Cart,on_delete=models.CASCADE)
-    status              =models.CharField(max_length=120,default='created')
-    shipping_total      = models.DecimalField(default=5.00, max_digits=100, decimal_places=2)
-    total               = models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
-
+    objects = OrderManager()
 
 
     def __str__(self):
@@ -48,6 +66,9 @@ def pre_save_create_order_id(sender, instance, *args, **kwargs):
     if not instance.order_id:
         print("inside if preorder")
         instance.order_id = unique_order_id_generator(instance)
+    qs = Order.objects.filter(cart=instance.cart).exclude(billing_profile=instance.billing_profile)
+    if qs.exists():
+        qs.update(active=False)
 
 
 def post_save_cart_total(sender, instance,created, *args, **kwargs):
